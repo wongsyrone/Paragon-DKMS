d1d5bda4277cf73ea22ab4450e3d9ecbf29b0575
Syrone Wong <wong.syrone@gmail.com>
2014-05-13 16:44:18 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Paragon UFSD driver with DKMS support
+
+##### Support DKMS framework on Ubuntu 12.04 LTS, the only thing you need is Express(FREE) version
+
+## Usage
+
+**MUST EXECUTE ./uninstall.sh FIRST!!!**
+
+    $ chmod +x install.sh
+    # ./install.sh
+
+---
+
+    $ chmod +x uninstall.sh
+    # ./uninstall.sh
+    
---
+++ uninstall.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -e
+set +x
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $SCRIPTDIR
+
+# load_file_version()
+source VERSION
+
+if ! [ $(id -u) = 0 ]; then
+	echo "This script must be run as root" 1>&2
+	exit 1
+fi
+
+pkgname=paragon-ufsd
+pkgdir=${pkgname}-${pkgver}
+
+ufsd_status=$(dkms status -m ${pkgname} -v ${pkgver})
+if ! [ "$ufsd_status" == ""  ]; then
+    rmmod -f -v -w ufsd
+    rmmod -f -v -w jnl
+    dkms uninstall -m ${pkgname} -v ${pkgver}
+	dkms remove -m ${pkgname} -v ${pkgver} --all
+	rm -rf /var/lib/dkms/${pkgname}
+	rm -rf /usr/src/${pkgdir}
+	rm -rf ${pkgdir}
+fi
+
+# remove kernel module again if something went wrong
+ufsd_module_status=$(lsmod | grep -i "ufsd")
+if ! [ "$ufsd_module_status" == "" ]; then
+    rmmod -f -v -w ufsd
+    rmmod -f -v -w jnl
+fi
+
+# remove previous added loading module and mount entry  
+sed -i '/ufsd/d' /etc/modules
+sed -i '/ufsd/d' /etc/fstab
+
+# remount devices via /etc/fstab
+depmod -a
